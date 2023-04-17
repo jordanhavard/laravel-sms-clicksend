fed8eddde3f0686e9248f67cfaa918c609c62d94
Jordan Havard <sirj.havard@gmail.com>
2023-04-17 16:33:35 +1000

    Resolve conflicts

--- src/ClickSendApi.php
+++ src/ClickSendApi.php
@@ -57,22 +57,6 @@ class ClickSendApi
         return $return;
     }
 
-    public function sendSms($from, $to, $message, $custom = null, $delay = null)
-    {
-        $sms = (new ClickSendMessage($message))->from($from)->to($to)->delay($delay)->custom($custom);
-        
-        $return = $this->sendManySms([$sms]);
-        // Keep the same output signature as previous iterations of sendSms(...) so we don't
-        // introduce breaking changes
-        $return["data"] = $return["data"][0];
-        if ($return["success"]){
-            $return['message'] = 'Message sent successfully.';
-        }
-        unset($return["failures"]);
-
-        return $return;
-    }
-
     /**
      * @param  array  $messages - array of ClickSendMessage to send
      * @return array
@@ -95,15 +79,6 @@ class ClickSendApi
                 'body' => $sms->content,
                 'schedule' => $sms->delay,
                 'custom_string' => $sms->custom,
-            ];
-        }
-
-            $payload['messages'][] = [
-                "from"      => $sms->from,
-                "to"        => $sms->to,
-                "body"      => $sms->content,
-                "schedule"  => $sms->delay,
-                "custom_string"  => $sms->custom,
             ];
         }
         
